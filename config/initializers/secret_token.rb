22a27273073aed7a89d94b77d11cc4888abceffb
Hipolito Lopez <hlopezrd@gmail.com>
2012-08-01 15:03:12 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ramtec::Application.config.secret_token = 'd944b9afb1ca5ad84820bfee5eee4fc7e975b6aacc84ef29fb811179dcfbd5bf02e9ce58cd60617aca6c9bd8d5eb4c8c1723f8faa411290270a8faab22960e1c'
