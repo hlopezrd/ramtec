8fe327a4e75c0d3ef61624d635e58cc7c6835163
Hipolito Lopez <hlopezrd@gmail.com>
2012-08-02 14:47:53 -0400

    Exercises from lesson 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,30 @@
 require 'spec_helper'
 
 describe "Static pages" do
+	
+  let(:base_title) { "RAMTEC PortalWeb" }
 
-  describe "Home page" do
+  describe "Pagina de Inicio" do
     
     it "should have the content 'RAMTEC'" do
       visit '/static_pages/home'
       page.should have_content('RAMTEC')
     end
     
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title}")
+    end
+    
     it "should have the title 'Inicio'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "RAMTEC PortalWeb | Inicio")
+                        :text => '| Inicio')
     end
   end
   
-  describe "Help page" do
+  describe "Pagina de Ayuda" do
 
     it "should have the content 'Ayuda'" do
       visit '/static_pages/help'
@@ -26,11 +34,11 @@ describe "Static pages" do
     it "should have the title 'Ayuda'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "RAMTEC PortalWeb | Ayuda")
+                        :text => "#{base_title} | Ayuda")
     end
   end
   
-  describe "About page" do
+  describe "Pagina de Acerca De" do
 
     it "should have the content 'Acerca De'" do
       visit '/static_pages/about'
@@ -40,8 +48,22 @@ describe "Static pages" do
     it "should have the title 'Acerca De'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "RAMTEC PortalWeb | Acerca De")
+                    :text => "#{base_title} | Acerca De")
     end
+  end
+  
+  describe "Pagina de Contacto" do
+  	
+  	it "should have the content 'Contacto'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contacto')
+    end
+    
+    it "should have the title 'Contacto'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "#{base_title} | Contacto")
+    end    
   end
   
 end
