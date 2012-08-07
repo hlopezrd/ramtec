2417dcc00f29351f3e97fe865d9c99ce35f776f0
Hipolito Lopez <hlopezrd@gmail.com>
2012-08-07 15:34:53 -0400

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Bienvenido al Portal Web de RAMTEC!"
       redirect_to @user
     else
